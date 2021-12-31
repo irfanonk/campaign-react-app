eaa562c9b84f9ebb7d095579ea52865f3bf7b5fa
irfan önk <57913715+irfanonk@users.noreply.github.com>
2021-12-31 16:32:54 +0300

    token creation

---
+++ src/eth/scripts/deploy.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Token = await ethers.getContractFactory("CreateToken");
+  token = await Token.deploy("irfan", "irf", "irf101", 1000);
+  await token.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
